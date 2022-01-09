6847933b51bdd7a13db01c515baa5b620c1bb460
Judickaelle <judickaelle.pelletier@gmail.com>
2022-01-09 15:20:58 +0100

    quit game possible

--- end_project_chauvigne_pelletier.py
+++ end_project_chauvigne_pelletier.py
@@ -3,6 +3,7 @@ import time
 
 def mainMenu() :
     valideChoice = False
+    #the user will be asked to choose what he want to do 
     while(not valideChoice):
         try :
             mainMenuChoice = int(input("\nPlease choose a valid item : \
@@ -10,10 +11,22 @@ def mainMenu() :
             \n2- Statistic\
             \n3- Quit\
             \n\nPlease enter your choice number : "))
-            valideChoice = (mainMenuChoice == 1 or mainMenuChoice == 2)
+            valideChoice = (mainMenuChoice == 1 or mainMenuChoice == 2 or mainMenuChoice == 3)
         except(ValueError):
             print("\n***********An error occurs, please make another choice**********")
             time.sleep(1)
+    #according to the user answer the programm continue running
+    match mainMenuChoice:
+        case 1:
+            print("Memory Game")
+        case 2:
+            print("Statistic Lobby")
+        case 3:
+            print("\nWe hope to see you soon again")
+            time.sleep(1.5)
+            quit()
+        case _:
+            print("Didn't match a case")    
 
 ######################################################
 ##################### MAIN ###########################
@@ -24,4 +37,4 @@ try :
     mainMenu()
 except(KeyboardInterrupt):
     print("\nWe hope to see you soon again")
-    time.sleep(1.5) #the programm will stop after 1.5s
+    time.sleep(1.5) #the programm freeze during 1.5s
